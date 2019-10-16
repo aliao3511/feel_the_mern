ec2b725d309ca2d75f986de0733c4a5b8dcfd83d
Michael Schwartz <alexandraliao3511@gmail.com>
2019-10-16 13:49:55 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+const app = express();
+const db = require("./config/keys").mongoURI;
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("mongoDB connected successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("we out here"));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
