32182e3240bb144d3d04f0215acfad1c33cdab68
Michael Schwartz <alexandraliao3511@gmail.com>
2019-10-16 15:03:32 -0400

    add login routes

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
-const app = express();
 const db = require("./config/keys").mongoURI;
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("mongoDB connected successfully"))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
 app.get("/", (req, res) => res.send("we out here"));
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,96 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
-router.get('/test', (req, res) => res.json({ msg: 'users' }));
+// router.get('/test', (req, res) => res.json({ msg: 'users' }));
+
+// registering a new user
+router.post('/register', (req, res) => {
+
+    // const { errors, isValid } = validateRegisterInput(req.body);
+
+    // if (!isValid) {
+    //     return res.status(400).json(errors);
+    // }
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'email already registered' });
+            } else {
+                const newUser = new User({
+                    handle: req.body.handle,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => {
+                                // res.json(user)
+                                const payload = { id: user.id, name: user.handle };
+                                jwt.sign(
+                                    payload,
+                                    keys.secretOrKey,
+                                    { expiresIn: 3600 },
+                                    (err, token) => {
+                                        res.json({
+                                            success: true,
+                                            token: 'Bearer ' + token,
+                                        });
+                                });
+                            })
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+});
+
+// logging in a user
+router.post('/login', (req, res) => {
+
+    // const { errors, isValid } = validateLoginInput(req.body);
+
+    // if (!isValid) {
+    //     return res.status(400).json(errors);
+    // }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'no account associated with this email'} );
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // res.json({ msg: 'logged in!!' });
+                        const payload = { id: user.id, name: user.handle };
+                        jwt.sign(
+                            payload, 
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token,
+                                });
+                            });
+                    } else {
+                        return res.status(400).json({ msg: 'incorrect password' });
+                    }
+                });
+        });
+});
 
 module.exports = router;
