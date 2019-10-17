7857f49ccb5675846c167feb5495514f586544c5
Alexandra Liao <alexandraliao3511@gmail.com>
2019-10-17 15:36:34 -0400

    add tweet model and validation

---
+++ validation/tweets.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateTweetInput(data) {
+    let errors = {};
+
+    data.body = validText(data.body) ? data.body : '';
+
+    if (!Validator.isLength(data.body, { min: 5, max: 140 })) {
+        errors.body = 'tweet must be between 5 and 140 characters';
+    }
+
+    if (Validator.isEmpty) {
+        errors.body = 'tweet cannot be blank';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0,
+    };
+};
+
