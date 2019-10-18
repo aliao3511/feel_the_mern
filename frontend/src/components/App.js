b6dd145ac53b64b357ed468bda519ec4a3e5781a
Alex Liao <alexandraliao3511@gmail.com>
2019-10-18 17:57:06 -0400

    add app component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,18 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
 import MainPage from './mainpage.js';
+// import NavBarContainer from '../components/nav/navbar_container';
+// import LoginFormContainer from '../components/session/login_container';
+// import SignupFormContainer from '../components/session/signup_container';
 
 const App = () => (
-  <Switch>
-    <AuthRoute exact path='/' component={MainPage} />
-  </Switch>
+  <div>
+    <Switch>
+      <AuthRoute exact path='/' component={MainPage} />
+      <AuthRoute exact path='/login' component={LoginFormContainer}/>
+      <AuthRoute exact path='/signup' component={SignupFormContainer}/>
+    </Switch>
+  </div>
 )
 
 export default App;
