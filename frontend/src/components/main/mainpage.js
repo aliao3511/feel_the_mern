6f4555a3930123b6289041fd84488b80ed4466af
Alex Liao <alexandraliao3511@gmail.com>
2019-10-18 15:44:08 -0400

    add mainpage component

---
+++ frontend/src/components/main/mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Mainpage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>a tweeter clone</h1>
+                <footer>
+                    copyright &copy a/L
+                </footer>
+            </div>
+        )
+    }
+};
