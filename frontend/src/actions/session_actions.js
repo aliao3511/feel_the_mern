21e4797ca062e0793b5ea7a0affee3f6a42903b7
Alex Liao <alexandraliao3511@gmail.com>
2019-10-28 18:33:29 -0400

    add login form component

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -2,10 +2,27 @@ import * as SessionAPIUtil from '../util/session_api_util';
 import jwt_decode from 'jwt-decode';
 
 export const RECEIVE_USER_LOGOUT = 'RECEIVE_USER_LOGOUT';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_USER_SIGN_IN = 'RECEIVE_USER_SIGN_IN';
 
 // action creators
 export const logoutUser = () => ({
     type: RECEIVE_USER_LOGOUT,
+});
+
+export const receiveSessionErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
+
+export const receiveCurrentUser = currentUser => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+});
+
+export const receiveUserSignin = () => ({
+    type: RECEIVE_USER_SIGN_IN,
 });
 
 // thunk action creators
@@ -13,5 +30,26 @@ export const logout = () => dispatch => {
     localStorage.removeItem('jwtToken');
     SessionAPIUtil.setAuthToken(false);
     dispatch(logoutUser());
+};
+
+export const login = user => dispatch => {
+    SessionAPIUtil.login(user).then(res => {
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        SessionAPIUtil.setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(receiveCurrentUser(decoded));
+    })
+    .catch(err => {
+        dispatch(receiveErrors(err.response.data));
+    });
+};
+
+export const signup = user => dispatch => {
+    SessionAPIUtil.signup(user).then(() => (
+        dispatch(receiveUserSignin())
+    ), err => (
+        dispatch(receiveErrors(err.response.data))
+    ));
 };
 
